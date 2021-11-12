09e144f9c7d570bf2c9586f1ae373e934c023729
Your Name <graceliu@gracelius-iMac.home>
2021-11-12 14:11:42 -0500

    mvp for generate joke,imge

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,24 +3,31 @@ var cardContainerEl = document.getElementById("card-container");
 var receiverTextEl = document.getElementById("receiver-text");
 var senderTextEl = document.getElementById("senderText");
 var addImageBtn = document.querySelector("#add-image");
-console.log(addImageBtn)
-var createBtnHandler =function(){
-    event.preventDefault();
-getJoke();
-//getImage();
-createCard();
-}
+var imgUrl = "";
+var jokeSetup ="";
+var jokeDelivery ="";
 
-var getJoke = function(category){
-    var apiUrl = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas";
-
+var getJoke = function(){
+    var apiJokeUrl = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Spooky,Christmas";
+console.log(apiJokeUrl)
     //make a request to the url
-    fetch(apiUrl)
+    fetch(apiJokeUrl)
     .then(function(response){
     if(response.ok){
         response.json().then(function(data){
-            console.log(data.setup);
-            console.log(data.delivery);
+            console.log(data)
+            console.log(data.type)
+            if(data.type === "twopart"){
+                console.log("twopart")
+            jokeSetup = data.setup;
+            console.log(data.setup)
+            jokeDelivery = data.delivery;
+            console.log(jokeDelivery)
+             } else {
+                 console.log("single")
+            jokeSetup = data.joke 
+            jokeDelivery = '';
+        console.log(jokeSetup)}
         })
     } else {
         alert("Poops, something went catAssTrophic" );
@@ -28,16 +35,20 @@ var getJoke = function(category){
 })
 }
 var addImageHandler = function(event){
-    event.preventDefault();
+   event.preventDefault();
     var category = event.target.getAttribute("data-category")
-    console.log(category);
-    var apiUrl ="https://some-random-api.ml/img/" + category
+    apiUrl ="https://some-random-api.ml/img/" + category
+if(!category){
+    apiUrl ="https://some-random-api.ml/img/cat" 
+}
     console.log (apiUrl)
     //make a request to the url
     fetch(apiUrl).then(function(response){
     if(response.ok){
         response.json().then(function(data){
-            createCard(data.link);
+            console.log(data.link);
+            imgUrl = data.link;
+            console.log();
         })
     } else {
         alert("Poops, something went catAssTrophic");
@@ -45,11 +56,22 @@ var addImageHandler = function(event){
 })
 }
 
-var createCard = function(){
+var generateCard = function(event){
+    event.preventDefault();
+    getJoke();
+console.log("here")
+cardContainerEl.textContent ="";
 var recipientName = document.createElement("p")
 recipientName.className = "h2";
-recipientName.textContent= "Dear " + receiverTextEl.value.trim();
+recipientName.textContent= "Dear " + receiverTextEl.value.trim()
 cardContainerEl.append(recipientName);
+var addImg = `<img src="${imgUrl}"/>`;
+console.log(addImg);
+var jokeContent = document.createElement("p");
+jokeContent.className = "p";
+jokeContent.innerHTML = jokeSetup + '<br>' + jokeDelivery +'<br>'+ "<p>" + addImg + "<p>" ;
+cardContainerEl.append(jokeContent);
+console.log(jokeSetup+ jokeDelivery);
 var senderName = document.createElement("p")
 senderName.className ="h3";
 senderName.textContent = "From " + senderTextEl.value.trim();
@@ -58,5 +80,5 @@ cardContainerEl.append(senderName);
 }
 
 
-makeYourCardEl.addEventListener("click", createBtnHandler)
-addImageBtn.addEventListener("click", addImageHandler);
+makeYourCardEl.addEventListener("click", generateCard)
+addImageBtn.addEventListener("click", addImageHandler)
