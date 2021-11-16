e361de7f4ccc0cdc99292a894780c2f0524254c7
Your Name <graceliu@gracelius-iMac.home>
2021-11-16 11:44:31 -0500

    fixed event delegation btns and used local storage to save and display last searched joke

--- assests/js/script.js
+++ assests/js/script.js
@@ -6,9 +6,11 @@ var getJokeBtn =document.getElementById("get-joke-btn");
 var cardContainerEl = document.getElementById("card-container");
 var receiverTextEl = document.getElementById("receiver-text");
 var senderTextEl = document.getElementById("senderText");
-var addImageBtn = document.querySelector("#add-image");
-var addJokeBtn = document.getElementById("occasion");
-console.log(addJokeBtn)
+var addImageEl = document.getElementById("add-image")
+var addImageBtn = document.querySelector(".add-image");
+var addJokeEl = document.querySelector("#add-occasion");
+var cardTypeBtn =document.querySelector(".card-type")
+console.log(cardTypeBtn)
 var occasionTextEl = document.getElementById("occasion-text")
 var saveBtn = document.querySelector("#save-button");
 var receiverNameEl =document.getElementById("receiver-name");
@@ -21,15 +23,17 @@ var jokeHistory=[];
 var imgUrl = "";
 var jokeSetup ="";
 var jokeDelivery ="";
+var jokeSetupText="";
+var jokeDeliveryText="";
 var loader = document.querySelector(".loader")
 var jokeHistoryEl =document.getElementById("joke-history")
+
 var getJoke = function(event){
     event.preventDefault();
     if(randomNumber(1,2) === 1){
     var apiJokeUrl = "https://v2.jokeapi.dev/joke/Programming,Christmas";}
     else{
     var apiJokeUrl ="https://api.icndb.com/jokes/random"}
-
     //make a request to the url
     fetch(apiJokeUrl)
     .then(function(response){
@@ -42,7 +46,6 @@ var getJoke = function(event){
             jokeSetup = data.setup;
             jokeDelivery = data.delivery;
              } if(data.type === "single") {
-        
             jokeSetup = data.joke 
             jokeDelivery = '';
              } if(data.type === "success"){
@@ -51,21 +54,24 @@ var getJoke = function(event){
              }
        writeJoke()
         })
-   
     } else {
         alert("Poops, something went catAssTrophic" );
     }
 })
 }
+
 var writeJoke = function(){
     jokeTextEl.textContent=""
-    var jokeSetupText = document.createElement("p")
+    jokeSetupText = document.createElement("p")
     jokeSetupText.textContent = jokeSetup;
     console.log(jokeSetupText)
     jokeTextEl.append(jokeSetupText);
-    var jokeDeliveryText = document.createElement("p")
+    jokeDeliveryText = document.createElement("p")
     jokeDeliveryText.textContent = jokeDelivery;
-    jokeTextEl.append(jokeDeliveryText)
+    jokeTextEl.append(jokeDeliveryText);
+    saveJoke();
+}
+var saveJoke =function(){
     //create jokeToPush as an object {} rather than an array []
     var jokeToPush ={};
     //add properties to object
@@ -89,7 +95,6 @@ var reWriteJoke =function(){
     console.log(savedJokeArr[0].delivery);
     for (var i=0; i<savedJokeArr.length;i++){
         jokeTextEl.textContent="";
-       
         var jokeSetupText = document.createElement("p")
         jokeSetupText.textContent = savedJokeArr[i].setup;
         console.log(savedJokeArr[i].setup)
@@ -118,37 +123,31 @@ var addImageHandler = function(event){
             imgUrl = "./assests/images/birthday-" + randomNumber(1,14)+".jpg"
         }
 writeImg()}
-
     var writeImg = function(event){
     console.log(imgUrl)
     var placeholderImg=document.getElementById("placeholder-img")
     console.log(placeholderImg.src)  
 placeholderImg.src =`${imgUrl}`
 console.log(placeholderImg.src)
-
 var addImg = `<img src="${imgUrl}"/>`;
-console.log(addImg)
-// var imgContainer = document.createElement("p");
-// imgContainer.append(addImg);
-// cardImageEl.parentNode.replaceChild(imgContainer,cardImageEl)
     }
 
-
 getJokeBtn.addEventListener("click", getJoke)
-addImageBtn.addEventListener("click", addImageHandler)
-$(addJokeBtn).on("click",".cardType",getJoke)
-$(document).on("load",".card-img",function(){
-    $(loader).classList.add(".disappear")
-})
+
+$(addImageEl).on('click', '.add-image',
+        addImageHandler)
+
+$(addJokeEl).on('click', '.card-type', getJoke)
+
 receiverTextEl.addEventListener("keyup",function(event){
     receiverNameEl.innerText = event.target.value
-   
 })
 senderTextEl.addEventListener("keyup",function(event){
     senderNameEl.innerText = event.target.value
 })
-addJokeBtn.addEventListener("click", function(event){
+$(addJokeEl).on('click', '.card-type',function(event){
  occasionTextEl.innerText = event.target.textContent
+
 })
 
 saveBtn.addEventListener('click', function(event) {
@@ -178,3 +177,22 @@ jokeHistoryEl.addEventListener("click",reWriteJoke)
 document.documentElement.addEventListener("click", function(){
     saveBtn.style.display="block"
 })
+$(document).ready(function() {
+    var searchJokeArr = JSON.parse(localStorage.getItem("joke"));
+    console.log(searchJokeArr);
+    if (searchJokeArr !== null) {
+        var lastJokeIndex = searchJokeArr.length - 1;
+        var lastJokeSetup = searchJokeArr[lastJokeIndex].setup;
+        var lastJokeDelivery = searchJokeArr[lastJokeIndex].delivery;
+        console.log(`Last joke: ${lastJokeSetup}`);
+        console.log(`Last joke: ${lastJokeDelivery}`);
+        jokeTextEl.textContent=""
+        jokeSetupText = document.createElement("p")
+        jokeSetupText.textContent = lastJokeSetup;
+        console.log(jokeSetupText)
+        jokeTextEl.append(jokeSetupText);
+        jokeDeliveryText = document.createElement("p")
+        jokeDeliveryText.textContent = lastJokeDelivery;
+        jokeTextEl.append(jokeDeliveryText);
+    }
+});
